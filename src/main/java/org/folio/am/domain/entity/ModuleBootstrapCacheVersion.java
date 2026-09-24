452b955c3c7b944691eae257f8e02860ea2bfd6d
Factory <factory@localhost>
2026-09-24 11:01:47 +0000

    Address PR feedback (round 1)

--- src/main/java/org/folio/am/domain/entity/ModuleBootstrapCacheVersion.java
+++ src/main/java/org/folio/am/domain/entity/ModuleBootstrapCacheVersion.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "module_bootstrap_cache_version")
-public class ModuleBootstrapCacheVersionEntity {
+public class ModuleBootstrapCacheVersion {
   @Id
   private Long id;
   private long version;
--- src/main/java/org/folio/am/repository/ModuleBootstrapCacheVersionRepository.java
+++ src/main/java/org/folio/am/repository/ModuleBootstrapCacheVersionRepository.java
@@ -1,18 +1,18 @@
 package org.folio.am.repository;
 
-import org.folio.am.domain.entity.ModuleBootstrapCacheVersionEntity;
+import org.folio.am.domain.entity.ModuleBootstrapCacheVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ModuleBootstrapCacheVersionRepository extends JpaRepository<ModuleBootstrapCacheVersionEntity, Long> {
+public interface ModuleBootstrapCacheVersionRepository extends JpaRepository<ModuleBootstrapCacheVersion, Long> {
 
-  @Query("select version from ModuleBootstrapCacheVersionEntity where id = 1")
+  @Query("select version from ModuleBootstrapCacheVersion where id = 1")
   long getVersion();
 
   @Modifying
-  @Query("update ModuleBootstrapCacheVersionEntity set version = version + 1 where id = 1")
+  @Query("update ModuleBootstrapCacheVersion set version = version + 1 where id = 1")
   int incrementVersion();
 }
